7dae81fb8ca35ebae1af38edd47fa93ed9198b2f
Mark.Etzel@gmx.email <Mark.Etzel@gmx.de>
2020-10-05 11:23:45 +0200

    intermediate finished

--- intermediate/myscript.js
+++ intermediate/myscript.js
@@ -6,3 +6,21 @@ $("form").submit(function(e) {
 	
 
 });
+
+// 1.Remove the form from the page
+// $("#myform").remove();
+
+// 2. Create 5 new DIVs in the article element
+for (let i = 0; i < 5; i++) {
+	$("article").append('<div>new div</h1>');
+}
+
+// 3. Within the input field change the value to "Search for..."
+$("input").val("Search for...");
+
+// 4. Add a class name of "box" to each new DIV
+$("article div").attr("class", "newClass");
+// alternatively: $("article div").toggleClass("newClass")
+
+// 5. Change the link to "www.codefactory.wien"
+$("a[href]").attr("href", "https://codefactory.wien/de/home/");
